690d6da20c8961207f5423bfdda1675796ce057c
yunseokim119 <yunseo7179@naver.com>
2025-04-02 11:28:49 +0900

    [feat]초기 셋팅 완료

--- project/app.js
+++ project/app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 
-dotenv.config(); // .env 파일 불러오기
+dotenv.config();
 
 const app = express();
 
-app.use(express.json()); // JSON 파싱 미들웨어
-app.use('/api/auth', authRoutes); // 인증 라우터
+app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
